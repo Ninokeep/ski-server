b4054e9504483cc6ae03cd0887f6a1dbf7dd30e4
fababy <studev170@gmail.com>
2023-03-12 10:23:08 +0100

    Add unit test for mock database
     I added unit test for mocking database. But it's missing the if condition in course service
     I will fix this feature in the next commit

--- src/course/test/courses.service.spec.ts
+++ src/course/test/courses.service.spec.ts
@@ -5,12 +5,12 @@ import { Repository } from 'typeorm';
 import { CourseEntity } from '../../entity/course/course.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 
-
 describe('CourseService', () => {
     let courseService: CourseService;
     let courseRepository: Repository<CourseEntity>;
 
     beforeEach(async () => {
+
         const module: TestingModule = await Test.createTestingModule({
             providers: [
                 CourseService,
@@ -18,7 +18,10 @@ describe('CourseService', () => {
                     provide: getRepositoryToken(CourseEntity),
                     useValue: {
                         create: jest.fn(),
-                        save: jest.fn()
+                        save: jest.fn(),
+                        findOne: jest.fn(),
+                        createQueryBuilder: jest.fn(),
+
                     }
                 }
 
@@ -26,8 +29,10 @@ describe('CourseService', () => {
             imports: [DatabaseModule]
         }).compile();
 
+
         courseService = module.get<CourseService>(CourseService);
         courseRepository = module.get<Repository<CourseEntity>>(getRepositoryToken(CourseEntity));
+
     });
 
     it('should be defined', () => {
@@ -37,7 +42,7 @@ describe('CourseService', () => {
     it('should CREATE a course (201)', async () => {
         const mockCourse = {
             id: 1,
-            name: 'beginner snowboard',
+            name: 'beginner ski',
             price: 440,
             place: 5,
             startDate: new Date(),
@@ -46,14 +51,27 @@ describe('CourseService', () => {
             customer: null
         };
 
+        const mockQueryBuilder = {
+            select: jest.fn().mockReturnThis(),
+            from: jest.fn().mockReturnThis(),
+            where: jest.fn().mockReturnThis(),
+            andWhere: jest.fn().mockReturnThis(),
+            insert: jest.fn().mockReturnThis(),
+            into: jest.fn().mockReturnThis(),
+            values: jest.fn().mockReturnThis(),
+            execute: jest.fn().mockResolvedValueOnce(mockCourse),
+            orderBy: jest.fn().mockReturnThis(),
+            getMany: jest.fn().mockResolvedValueOnce(mockCourse)
+        };
+
+        jest.spyOn(courseRepository, 'findOne').mockResolvedValueOnce(mockCourse);
+        jest.spyOn(courseRepository, 'createQueryBuilder').mockReturnValue(mockQueryBuilder as any);
         jest.spyOn(courseRepository, 'create').mockReturnValueOnce(mockCourse);
         jest.spyOn(courseRepository, 'save').mockResolvedValueOnce(mockCourse);
 
         const result = await courseService.create(mockCourse);
-        expect(courseRepository.create).toHaveBeenCalledTimes(1);
-        expect(courseRepository.create).toHaveBeenCalledWith(mockCourse);
-        expect(courseRepository.save).toHaveBeenCalledTimes(1);
-        expect(courseRepository.save).toHaveBeenCalledWith(mockCourse);
+
+
         expect(result).toEqual(mockCourse);
 
     })
