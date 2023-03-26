c2a875f0828d4fa712f67893d1f70d10ddac107a
fababy <studev170@gmail.com>
2023-03-26 13:57:44 +0200

    Add passport, module auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { CourseModule } from './course/course.module';
 import { DatabaseModule } from './configuration/database/database.module';
 import { MonitorModule } from './monitor/monitor.module';
+import { AuthModule } from './auth/module/auth.module';
+import { AuthController } from './auth/controller/auth.controller';
+import { AuthService } from './auth/service/auth.service';
 
 @Module({
-  imports: [CourseModule, DatabaseModule, MonitorModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CourseModule, DatabaseModule, MonitorModule, AuthModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
-export class AppModule {}
+export class AppModule { }
